949e566dae9ae9b64d7fa3d867de03ce85e29971
Bernhard Ertler <bernie@sternpunkt.at>
2018-04-25 23:02:58 +0200

    provisioning for kokadi.de

--- public/provisioning/setup/apache.sh
+++ public/provisioning/setup/apache.sh
@@ -13,6 +13,17 @@ else
     sudo a2dissite 421-fmoser.conf
 fi
 
+
+# kokadi.de
+# -------------------
+sudo cp /usr/local/vagrant/hosts/422-kokadi.conf /etc/apache2/sites-available/
+
+if [ -d "/var/www/kokadi.de" ]; then
+    sudo a2ensite 422-kokadi.conf
+else
+    sudo a2dissite 422-kokadi.conf
+fi
+
 # easy-holiday.at
 # -------------------
 # sudo cp /usr/local/vagrant/hosts/005-easyholiday.conf /etc/apache2/sites-available/
